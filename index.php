8c6d5e0b52e1d3663bf3000ddf27261c15260be2
Didixe <Adeline.gilet@gmail.com>
2023-10-17 19:25:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '../vendor/autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+// $output = $bessie->say();
+// echo $output;
+
+$bessie->setTongue('U');
+
+// or just echo the object for direct output
+echo $bessie;
